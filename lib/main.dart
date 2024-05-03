dd3fff9f95918435788fdb9b70d47bfa61668f51
MUHAMMAD TALHA <tahakhan4141@gmail.com>
2024-05-03 22:56:15 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:to_do_app/screens/add_task.dart';
 import 'package:to_do_app/screens/loading_screen.dart';
 import 'package:to_do_app/screens/task_list.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(TO_DO_APP());
 }
 
